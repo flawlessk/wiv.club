734a2a22341204a76758b834edf99682ba28496e
Davit Kurdadze <davit.kurdadze@webiz.ge>
2023-05-23 02:37:15 +0400

    feat: start wiv.club project

--- index.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
-import App from './js/App';
-
-// const app = new App();
-
-AppRegistry.registerComponent('movieApp', () => App);
