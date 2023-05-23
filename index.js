61c88ec0ec2db89d31eba31e2d282fd8250262e6
Davit Kurdadze <73129826+flawlessk@users.noreply.github.com>
2023-05-23 15:52:55 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+import App from './js/App';
+
+// const app = new App();
+
+AppRegistry.registerComponent('movieApp', () => App);
